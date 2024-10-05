593759a39d22b9d8f9c67be613991fb8aeffe3f3
shamshod8052 <shamshodramazonov8052@gmail.com>
2024-10-05 10:30:58 +0500

    fix

--- user_bot/loader.py
+++ user_bot/loader.py
@@ -2,4 +2,4 @@ from telethon import TelegramClient
 
 from user_bot.config import NUMBER, API_ID, API_HASH
 
-client = TelegramClient(f"user_bot/sessions/{NUMBER}", API_ID, API_HASH)
+client = TelegramClient(f"user_bot/sessions/{NUMBER}", API_ID, API_HASH, sequential_updates=True)
