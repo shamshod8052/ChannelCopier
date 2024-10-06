3b33d12fdb40a44da670fec385f0d9d1da32db4c
shamshod8052 <shamshodramazonov8052@gmail.com>
2024-10-06 19:09:23 +0500

    fix

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from environs import Env
 
@@ -101,7 +102,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.static import serve
+from django.urls import re_path
+from django.conf.urls.static import static
+
+from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns += [re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
