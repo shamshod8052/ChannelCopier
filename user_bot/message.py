6c633c4e38df40eba2a2edf2e74614621e78ee14
shamshod8052 <shamshodramazonov8052@gmail.com>
2024-10-06 14:28:49 +0500

    fix user_bot

--- user_bot/message.py
+++ user_bot/message.py
@@ -15,8 +15,12 @@ async def get_for_send_channels():
 
 async def get_message_objs(chat_id, message_ids):
     try:
-        messages = Message.objects.filter(from_chat_id=chat_id, from_message_ids__icontains=message_ids)
+        ans = []
+        messages = Message.objects.filter(from_chat_id=chat_id)
+        for msg in messages.all():
+            if len(set(msg.from_message_ids) & set(message_ids)) == len(message_ids):
+                ans.append(msg)
     except Message.DoesNotExist:
         return None
     else:
-        return messages.all()
+        return ans
