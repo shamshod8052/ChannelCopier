d65584adf839209e1000350ce9ee81fd03c9bf47
shamshod8052 <shamshodramazonov8052@gmail.com>
2024-10-06 12:54:03 +0500

    fix

--- user_bot/handlers.py
+++ user_bot/handlers.py
@@ -9,20 +9,24 @@ from user_bot.loader import client
 
 @client.on(Album)
 async def handle_media_group(album: Album.Event):
+    print(album)
     await send_media_group(album)
 
 
 @client.on(NewMessage)
 async def handle_new_message(event: NewMessage.Event):
+    print(event)
     if not event.message.grouped_id:
         await forward(event)
 
 
 @client.on(MessageEdited)
 async def handle_edited_message(event: MessageEdited.Event):
+    print(event)
     await edit(event)
 
 
 @client.on(MessageDeleted)
 async def handle_deleted_message(event: MessageDeleted.Event):
+    print(event)
     await delete(event)
