2fda6c50db18c4a4308c4421caa31c022d5015e6
shamshod8052 <shamshodramazonov8052@gmail.com>
2025-03-09 10:12:44 +0500

    fix cleaning manager

--- cleaning.py
+++ cleaning.py
@@ -3,15 +3,14 @@ from datetime import timedelta
 
 from django.utils.timezone import now
 
-from Admin.models import Message
 
 
 def db_cleaning_manager():
     """Cleaner function for old db objects"""
+    from Admin.models import Message
+
     threshold_date = now() - timedelta(days=30)  # Delete data older than 30 days
-
     deleted_count, _ = Message.objects.filter(created_at__lt=threshold_date).delete()
-
     logging.info(f"Deleted {deleted_count} old messages")
 
     return deleted_count
