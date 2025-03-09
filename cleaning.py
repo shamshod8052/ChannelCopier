b48cda566afd890f63f908d39d6ab40f7d5edc24
shamshod8052 <shamshodramazonov8052@gmail.com>
2025-03-09 10:06:40 +0500

    add cleaning method

---
+++ cleaning.py
@@ -0,0 +1,17 @@
+import logging
+from datetime import timedelta
+
+from django.utils.timezone import now
+
+from Admin.models import Message
+
+
+def db_cleaning_manager():
+    """Cleaner function for old db objects"""
+    threshold_date = now() - timedelta(days=30)  # Delete data older than 30 days
+
+    deleted_count, _ = Message.objects.filter(created_at__lt=threshold_date).delete()
+
+    logging.info(f"Deleted {deleted_count} old messages")
+
+    return deleted_count
--- run_user_bot.py
+++ run_user_bot.py
@@ -1,26 +1,31 @@
 import asyncio
-import datetime
 import logging
 import sys
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from django.utils.timezone import now
 from telethon import TelegramClient
 
 from UserBot.config import PHONE_NUMBER, INTERVAL_SECONDS
 from UserBot.loader import client
+from cleaning import db_cleaning_manager
 
 
 async def on_startup_notify(client_: TelegramClient) -> None:
     me = await client_.get_me()
     fullname = me.first_name or '' + (f' {me.last_name}' if me.last_name else '')
     logging.info(f"Run polling for user @{me.username} id={me.id} - '{fullname}'")
-    logging.info(f"Running time {datetime.datetime.now()}")
+    logging.info(f"Running time {now()}")
 
 
 async def scheduler():
     from UserBot.copy_process import copy_manager
     schedule = AsyncIOScheduler(timezone='Asia/Tashkent')
     schedule.add_job(copy_manager, trigger='interval', seconds=INTERVAL_SECONDS, id='copier')
+    schedule.add_job(
+        db_cleaning_manager, trigger='cron', id='cleaner',
+        start_date=now(), hour=9, minute=00, second=0
+    )
     schedule.start()
 
 
@@ -30,7 +35,6 @@ async def main():
     await client.start(phone=PHONE_NUMBER)
     logging.info(f"Client started")
     await on_startup_notify(client)
-    from UserBot import handlers
     await scheduler()
     await client.run_until_disconnected()
 
