7c449bee344e026e1970995ed5103519ef90785c
Shamshod <shamshodramazonov8052@gmail.com>
2026-08-24 11:21:17 +0000

    add

--- UserBot/loader.py
+++ UserBot/loader.py
@@ -2,4 +2,8 @@ from telethon import TelegramClient
 
 from UserBot.config import PHONE_NUMBER, API_ID, API_HASH
 
-client = TelegramClient(f"UserBot/sessions/{PHONE_NUMBER}", API_ID, API_HASH)
+proxy = ('socks5', '127.0.0.1', 1080)
+
+client = TelegramClient(
+     f"UserBot/sessions/{PHONE_NUMBER}", API_ID, API_HASH, proxy=proxy
+)
--- cleaning.py
+++ cleaning.py
@@ -9,7 +9,7 @@ def db_cleaning_manager():
     """Cleaner function for old db objects"""
     from Admin.models import Message
 
-    threshold_date = now() - timedelta(days=30)  # Delete data older than 30 days
+    threshold_date = now() - timedelta(days=7)  # Delete data older than 7 days
     deleted_count, _ = Message.objects.filter(created_at__lt=threshold_date).delete()
     logging.info(f"Deleted {deleted_count} old messages")
 
